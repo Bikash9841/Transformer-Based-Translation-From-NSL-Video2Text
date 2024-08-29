8d14b71837566447ca43e9f51ef0280dd1e8c8f7
Bikash9841 <bikashojha904@gmail.com>
2024-08-29 16:37:32 +0545

    feat: implement beam inference technique

--- apps/model.py
+++ apps/model.py
@@ -17,6 +17,11 @@ from torchvision.transforms import (
 )
 
 
+'''
+Initialize of the model with trained weights and define helper functions.
+'''
+
+
 def get_config():
     return {
         "seq_len": 15,
@@ -114,6 +119,70 @@ def greedy_decode(model, src_video, source_mask, tokenizer_tgt, max_len, device)
     return decoder_input.squeeze(0)
 
 
+def beam_search_decode(model, beam_size, src_video, source_mask, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(src_video=src_video)
+    # Initialize the decoder input with the sos token
+    decoder_initial_input = torch.empty(1, 1).fill_(
+        sos_idx).type_as(src_video.type(torch.LongTensor)).to(device)
+
+    # Create a candidate list
+    candidates = [(decoder_initial_input, 1)]
+
+    while True:
+
+        # If a candidate has reached the maximum length, it means we have run the decoding for at least max_len iterations, so stop the search
+        if any([cand.size(1) == max_len for cand, _ in candidates]):
+            break
+
+        # Create a new list of candidates
+        new_candidates = []
+
+        for candidate, score in candidates:
+
+            # Do not expand candidates that have reached the eos token
+            if candidate[0][-1].item() == eos_idx:
+                continue
+
+            # Build the candidate's mask
+            candidate_mask = causal_mask(candidate.size(1)).type_as(
+                src_video.type(torch.LongTensor)).to(device)
+
+            # calculate output
+            out = model.decode(encoder_output=encoder_output,
+                               src_mask=None, tgt=candidate, tgt_mask=candidate_mask)
+
+            # get next token probabilities
+            prob = model.project(out[:, -1])
+
+            # get the top k candidates
+            topk_prob, topk_idx = torch.topk(prob, beam_size, dim=1)
+
+            for i in range(beam_size):
+                # for each of the top k candidates, get the token and its probability
+                token = topk_idx[0][i].unsqueeze(0).unsqueeze(0)
+                token_prob = topk_prob[0][i].item()
+                # create a new candidate by appending the token to the current candidate
+                new_candidate = torch.cat([candidate, token], dim=1)
+                # We sum the log probabilities because the probabilities are in log space
+                new_candidates.append((new_candidate, score + token_prob))
+
+        # Sort the new candidates by their score
+        candidates = sorted(new_candidates, key=lambda x: x[1], reverse=True)
+        # Keep only the top k candidates
+        candidates = candidates[:beam_size]
+
+        # If all the candidates have reached the eos token, stop
+        if all([cand[0][-1].item() == eos_idx for cand, _ in candidates]):
+            break
+
+    # Return the best candidate
+    return candidates[0][0].squeeze()
+
+
 def get_video_tensor(video_path):
     labeled_video_paths = [
         (video_path, {'label': 2})]
@@ -143,10 +212,13 @@ def run_inference(model, video, tokenizer_tgt, max_len, device):
         assert encoder_input.size(
             0) == 1, "Batch size must be 1 for validation"
 
-        model_out = greedy_decode(
-            model, encoder_input, None, tokenizer_tgt, max_len, device)
+        # model_out = greedy_decode(
+        #     model, encoder_input, None, tokenizer_tgt, max_len, device)
+
+        model_out_beam = beam_search_decode(
+            model, 2, encoder_input, None, tokenizer_tgt, max_len, device)
 
         model_out_text = tokenizer_tgt.decode(
-            model_out.detach().cpu().numpy())
+            model_out_beam.detach().cpu().numpy())
 
         return {'translation': model_out_text}
